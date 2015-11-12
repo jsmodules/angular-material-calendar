825a03d19dfccd2ab6f9552c30267e91cbd61e4b
Gavin Barron <Gavin.Barron@intergen.co.nz>
2015-11-12 15:03:17 -0800

    bug fixin

--- src/angular-material-calendar.js
+++ src/angular-material-calendar.js
@@ -205,8 +205,8 @@ angular.module("materialCalendar").directive("calendarMd", ["$compile", "$parse"
 
             $scope.isDisabled = function (date) {
                 if ($scope.disableFutureSelection && date > new Date()) { return true; }
-                return $scope.sameMonth(date);
-            }
+                return !$scope.sameMonth(date);
+            };
 
             $scope.calendarDirection = $scope.calendarDirection || "horizontal";
 
