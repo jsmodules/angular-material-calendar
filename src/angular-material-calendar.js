d75c9bfa954b5fd2d372d2056cb8732256fa791a
Diego Dal Bosco <diegodalbo97@gmail.com>
2016-06-09 16:04:51 +0200

    Using compile directive

--- src/angular-material-calendar.js
+++ src/angular-material-calendar.js
@@ -12,6 +12,20 @@ angular.module("materialCalendar").config(["materialCalendar.config", "$logProvi
     }
 }]);
 
+angular.module("materialCalendar").directive('compile', ['$compile', function ($compile) {
+    return function(scope, element, attrs) {
+        scope.$watch(
+            function(scope) {
+                return scope.$eval(attrs.compile);
+            },
+            function(value) {
+                element.html(value);
+                $compile(element.contents())(scope);
+            }
+        );
+    };
+}]);
+
 angular.module("materialCalendar").service("materialCalendar.Calendar", [function () {
 
     function Calendar(year, month, options) {
